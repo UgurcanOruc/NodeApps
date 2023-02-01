c46a6ca754ff8d4d46299d8acfa15b5ed9b78f9c
UgurcanOruc <ugurcan-oruc@hotmail.com>
2023-02-01 19:52:49 +0300

    hbs installed and views organized. also some css styling is made

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 
 RUN npm i nodemon@1.18.5 -g COMMAND AFTER DOWNLOADING
 
-TO RUN app.js the command is not 'node app.js', it is 'nodemon app.js' cause of nodemon.
+TO RUN app.js every save the command is not 'node app.js', it is 'nodemon app.js' cause of nodemon.
+TO RUN express with hbs extention saves, command is nodemon app.js -e js,hbs
 TO CLOSE NODEMON => ctrl + c
 TO DEBUG PROGRAM => node inspect OR node --inspect-brk 
--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,14 +1,62 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
+// Define paths for Express config
 const publicDirectory = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+
+// Setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+
+// Setup static directory to serve
 app.use(express.static(publicDirectory));
 
-app.get("/weather", (req, res) => {
-  res.send("Weather Page.");
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: 'Weather',
+    name: 'Ugurcan'
+  });
 });
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About",
+    name: "Ugurcan",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    message: "This is a message",
+    title: 'Help',
+    name: 'Ugurcan'
+  });
+});
+
+app.get('/help/*', (req, res) => {
+ res.render("404", {
+   message: "Help article not found!",
+   title: "404",
+   name: "Ugurcan",
+ });
+});
+
+app.get('*', (req, res) => {
+  res.render("404", {
+    message: "My 404 page.",
+    title: "404",
+    name: "Ugurcan",
+  });
+})
 
 app.listen(3000, () => {
     console.log('Server is up on port 3000.');
