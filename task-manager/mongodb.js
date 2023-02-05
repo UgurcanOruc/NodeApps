42dd7bb721ffad2a7ff7a73669536843e3345fce
UgurcanOruc <ugurcan-oruc@hotmail.com>
2023-02-05 15:33:30 +0300

    insert and insertMany functions and ObjectId is learned

--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,38 +1,94 @@
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const uri =
   "mongodb+srv://ugurcan:15935713579@cluster0.tggomuo.mongodb.net/?retryWrites=true&w=majority";
 const client = MongoClient(uri);
-const databaseName = 'task-manager';
+const databaseName = "task-manager";
 
 async function main() {
-    try {
-      await client.connect();
-      console.log("baglandi");
-        await createListing(client, {
-            name: 'Ugurcan Pruc',
-            age: 26
-        })
-    } catch (e) {
-      console.error("Unable to connect to database.");
-    } finally {
-      client.close();
-    }
+  try {
+    await client.connect();
+    console.log("Connected");
 
-    async function listDatabases(client) {
-      const dbList = await client.db().admin().listDatabases();
-      console.log("Databases:");
-      dbList.databases.forEach((db) => {
-        console.log(db.name);
-      });
-    }
+    await insertUser(client, {
+      _id: new ObjectId(),
+      name: "Vicdansiz Fikret",
+      age: 26,
+    });
+
+    // await insertUsers(client, [
+    //   {
+    //     name: 'Jen',
+    //     age: 28
+    //   },
+    //   {
+    //     name: 'Gunther',
+    //     age: 29
+    //   }
+    // ]);
+
+    // await insertTasks(client, [
+    //   {
+    //     description: 'Clean the house.',
+    //     completed: true
+    //   },
+    //   {
+    //     desvription: 'Renew inspection',
+    //     completed: false
+    //   },
+    //   {
+    //     description: 'Pot plants',
+    //     completed: false
+    //   }
+    // ]);
+  } catch (e) {
+    console.error("Unable to connect to database.");
+  } finally {
+    client.close();
+  }
+
+  async function listDatabases(client) {
+    const dbList = await client.db().admin().listDatabases();
+    console.log("Databases:");
+    dbList.databases.forEach((db) => {
+      console.log(db.name);
+    });
+  }
 }
 
-async function createListing(client, newListing) {
-    const result = await client
-      .db(databaseName)
-      .collection("users")
-      .insertOne(newListing);
-    console.log("New Listing id: " + result.insertedId);
+async function insertUser(client, user) {
+  client
+    .db(databaseName)
+    .collection("users")
+    .insertOne(user, (error, result) => {
+      if (error) {
+        return console.log("Unable to insert user.");
+      }
+      console.log(result.ops);
+    });
+}
+
+async function insertUsers(client, users) {
+  client
+    .db(databaseName)
+    .collection("users")
+    .insertMany(users, (error, result) => {
+      if (error) {
+        return console.log("Unable to insert users.");
+      }
+      console.log(result.ops);
+    });
+}
+
+async function insertTasks(client, tasks) {
+  client
+    .db(databaseName)
+    .collection("tasks")
+    .insertMany(tasks, (error, result) => {
+      if (error) {
+        return console.log("Unable to insert tasks.");
+      }
+      console.log(result.ops);
+    });
 }
 
 main().catch(console.error);
