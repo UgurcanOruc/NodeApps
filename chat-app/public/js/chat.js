3b0f96130448d8cdb8a3b3818893b2a4a688ee99
UgurcanOruc <ugurcan-oruc@hotmail.com>
2023-03-06 17:56:04 +0300

    join page created

--- chat-app/public/js/chat.js
+++ chat-app/public/js/chat.js
@@ -9,6 +9,8 @@ const $messages = document.querySelector("#messages");
 const messageTemplate = document.querySelector("#message-template").innerHTML;
 const locationMessageTemplate = document.querySelector("#location-message-template").innerHTML;
 
+const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true});
+
 socket.on("message", (message) => {
   const html = Mustache.render(messageTemplate, {
     message: message.text,
@@ -67,3 +69,5 @@ $sendLocation.addEventListener("click", () => {
     );
   });
 });
+
+socket.emit('join', { username, room });
--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const http = require("http");
 const socketio = require("socket.io");
-const Filter = require('bad-words');
+const Filter = require("bad-words");
 const {
   generateMessage,
   generateLocationMessage,
@@ -20,21 +20,25 @@ app.use(express.static(publicDirectoryPath));
 io.on("connection", (socket) => {
   console.log("New WebSocket connection");
 
-  socket.emit("message", generateMessage('Welcome!'));
-  socket.broadcast.emit("message", generateMessage("A new user has joined!"));
+  socket.on("join", ({ username, room }) => {
+    socket.join(room);
+
+    socket.emit("message", generateMessage("Welcome!"));
+    socket.broadcast.to(room).emit("message", generateMessage(`${username} has joined`));
+  });
 
   socket.on("sendMessage", (message, callback) => {
     const filter = new Filter();
 
     if (filter.isProfane(message)) {
-      return callback('Profanity is not alllowed.')
+      return callback("Profanity is not alllowed.");
     }
 
     io.emit("message", generateMessage(message));
     callback();
   });
 
-  socket.on('sendLocation', (coords, callback) => {
+  socket.on("sendLocation", (coords, callback) => {
     io.emit(
       "locationMessage",
       generateLocationMessage(
@@ -44,7 +48,7 @@ io.on("connection", (socket) => {
     callback();
   });
 
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     io.emit("message", generateMessage("A user has left"));
   });
 });
